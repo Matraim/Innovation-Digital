8e11b786a08f0c04e314086040e6f703799c158a
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-18 14:21:43 +0600

    delet-navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/navbar/NavBar';
 import { GithubChart } from './containers';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <GithubChart />
     </Router>
   );
